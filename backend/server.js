2aac29967a69e94fc0cd771d125f146bde15767d
gautamchandna5-alt <gautamchandna5@gmail.com>
2026-09-11 21:13:29 +0530

    created the backend structure
    today as the end of day 1 I've built a good backend fo rthe upcoming project

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import pool from './db.js';
+
+// cookies
+import cookieParser from 'cookie-parser';
+
+// routes
+import authRoutes from './routes/authRoute.js';
+
+
+dotenv.config({ path: './.env' });
+
+console.log('Database User Check:', process.env.DB_USER);
+
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ status: 'Server is running smoothly' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(pool.connect);
+});
